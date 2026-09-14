a340231046a8adfc5f663ff780b85c90bb5403e4
Marius Dollinger <mariusdollinger@icloud.com>
2026-09-14 16:45:40 +0200

    Change: SNOW_USER_LOOKUP_FIELD auf  "email"

--- app/core/config.py
+++ app/core/config.py
@@ -22,7 +22,7 @@ class Settings(BaseSettings):
     ASSET_NAME_LOOKUP_CONCURRENCY: int = 10
     ASSET_NAME_LOOKUP_LIMIT: int = 50
     DESCRIPTION_ASSET_PREVIEW: int = 10
-    SNOW_USER_LOOKUP_FIELD: str = "name"
+    SNOW_USER_LOOKUP_FIELD: str = "email"
 
     CATEGORY_MAP: Dict[str, str] = {
         "eu-elastic-hodgkin-413342": "Azure",
